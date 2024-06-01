a57f1e0d0c4f8b6b0d51bc4bc31883fdd6482f31
daBard <50872455+daBard@users.noreply.github.com>
2024-06-01 22:32:13 +0200

    bug fix

--- silicon-subscriberProvider.Infrastructure/Models/SubscriberResult.cs
+++ silicon-subscriberProvider.Infrastructure/Models/SubscriberResult.cs
@@ -1,11 +1,10 @@
-﻿namespace silicon_subscriberProvider.Infrastructure.Models
+﻿namespace silicon_subscriberProvider.Infrastructure.Models;
+
+public class SubscriberResult
 {
-    public class SubscriberResult
-    {
-        public SubscriberModel? Subscriber {  get; set; }
-        public IEnumerable<SubscriberModel>? Subscribers { get; set; }
-        public IEnumerable<string>? Emails { get; set; }
-        public string Status { get; set; } = null!;
-        public string? Error { get; set;}
-    }
+    public SubscriberModel? Subscriber {  get; set; }
+    public IEnumerable<SubscriberModel>? Subscribers { get; set; }
+    public IEnumerable<string>? Emails { get; set; }
+    public string Status { get; set; } = null!;
+    public string? Error { get; set;}
 }
